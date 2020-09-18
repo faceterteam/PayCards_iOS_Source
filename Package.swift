e298356db30a3b108fdaa37a718ec464cad2e0e0
Joshua Pierce <jpierce@myluckyday.fr>
2020-09-18 11:51:50 +0200

    updated artifact for arm64 in simulator build

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "PayCardsRecognizer",
             url: "https://github.com/therealmyluckyday/PayCards_iOS_Source/releases/download/1.1.7/PayCardsRecognizer.xcframework.zip",
-            checksum: "848089cd60219b7618d042fdad270cce7fcc8cc5461e51d5fb110e658f807eb7"
+            checksum: "7ed5d79d476c6ed5ccd7679b8b2a1db6c2c9a63962411d9f89f12a19863c38b3"
         )
     ],
     swiftLanguageVersions: [.v5]
